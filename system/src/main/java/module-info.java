7a7be92c22db1cc4ba5348e674bf654ebdbcfc19
ReesBendetti <rtblego@gmail.com>
2023-06-12 13:19:32 -0400

    The Whole Thing

--- system/src/main/java/module-info.java
+++ system/src/main/java/module-info.java
@@ -1,7 +1,16 @@
 module com.cardsystem {
     requires javafx.controls;
     requires javafx.fxml;
+    requires json.simple;
 
     opens com.cardsystem to javafx.fxml;
     exports com.cardsystem;
+
+    opens com.model to javafx.fxml;
+
+    exports com.model;
+
+    opens com.data to javafx.fxml;
+
+    exports com.data;
 }
