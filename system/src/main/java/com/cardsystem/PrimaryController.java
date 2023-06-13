6c85b04c48b5b9e9eb44a792966943104a954a0b
portiaportia <portiaplante@gmail.com>
2023-06-13 13:00:39 -0400

    resolving issues in primary.fxml

--- system/src/main/java/com/cardsystem/PrimaryController.java
+++ system/src/main/java/com/cardsystem/PrimaryController.java
@@ -8,12 +8,12 @@ import javafx.scene.control.Button;
 public class PrimaryController {
 
     @FXML
+    private Button primaryButton;
+
+    @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
     }
-
-        @FXML
-    private Button primaryButton;
 
     @FXML
     void switchToSecondary(ActionEvent event) {
