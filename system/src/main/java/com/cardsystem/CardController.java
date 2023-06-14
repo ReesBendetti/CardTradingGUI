662db173267a4a21c22b6753b86e9bdac2cfc60a
Mark <markbob666@gmail.com>
2023-06-14 13:55:34 -0400

    jordan

--- system/src/main/java/com/cardsystem/CardController.java
+++ system/src/main/java/com/cardsystem/CardController.java
@@ -13,12 +13,14 @@ public class CardController implements Initializable{
 
     @FXML
     private Text playerName;
+    @FXML private Text playerPosition;
 
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
         CardSystemFacade cardSystem = CardSystemFacade.getInstance();
         Card currentCard = cardSystem.getCurrentCard();
         playerName.setText(currentCard.getFirstName() + " " + currentCard.getLastName());
+        playerPosition.setText(currentCard.getPlayerPosition());
     }
     
 }
