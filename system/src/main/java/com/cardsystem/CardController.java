ed3abdcf27589c44bbb8d63bf0aa95cad8b2d36c
Mark <markbob666@gmail.com>
2023-06-14 14:12:03 -0400

    adding images

--- system/src/main/java/com/cardsystem/CardController.java
+++ system/src/main/java/com/cardsystem/CardController.java
@@ -2,6 +2,8 @@ package com.cardsystem;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
 
 import java.net.URL;
@@ -14,6 +16,7 @@ public class CardController implements Initializable{
     @FXML
     private Text playerName;
     @FXML private Text playerPosition;
+     @FXML private ImageView card_img;
 
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
@@ -21,6 +24,8 @@ public class CardController implements Initializable{
         Card currentCard = cardSystem.getCurrentCard();
         playerName.setText(currentCard.getFirstName() + " " + currentCard.getLastName());
         playerPosition.setText(currentCard.getPlayerPosition());
+         Image image = new Image(getClass().getResourceAsStream("/images/michaeljordan.png"));
+         card_img.setImage(image);
     }
     
 }
